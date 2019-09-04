26e90811a63df8e86e81318d66b3e35823e52945
Dustin Welu <leopardgecko37@gmail.com>
2019-09-04 17:07:43 -0500

    updated

--- project1/client.c
+++ project1/client.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]){
   char input1[2048] = "";
   char buffer[1] = "";
   int quit = 1;
+  /*char *multipleInputs[2048];*/
   /*creation of the socket file descriptor*/
   sockfd = socket(AF_INET,SOCK_STREAM,0);
   if(sockfd<0){
@@ -36,21 +37,48 @@ int main(int argc, char *argv[]){
   while(quit){
     /*get stdin from user*/
     read(STDIN_FILENO, input1, 2048);
-    /*exit loop when quit is entered*/
-    if(strcmp(input1,"quit\n")==0){
-      quit = 0;
-    }
-    /*write command to the socket to be sent to server*/
-    write(sockfd,input1,strlen(input1));
-    memset(input1,0,sizeof(input1));
-    /*read the redirected stdout and stderr from server*/
-    while(read(sockfd,buffer,1)>=0){
-      printf("%s",buffer);
-      if(strcmp(buffer,"\0")==0){
-	break;
+    /*this section of code is where the client parses multiple commands
+      if there are multiple commands present the client will loop through them
+      and send each of them to the server*/
+    int index = 0;
+    char nextWord[2048] = "";
+    char inputCpy[2048] = "";
+    memcpy(inputCpy,input1,2048);
+    while(inputCpy[index]!='\n'&&index<2048){
+      int nextIndex = 0;
+      while(inputCpy[index]!=';'&&inputCpy[index]!='\n'&&index<2048){
+	nextWord[nextIndex]=inputCpy[index];
+	index++;
+	nextIndex++;
       }
+      if(inputCpy[index]!='\n'){
+	index++;
+      }
+      nextWord[nextIndex]='\n';
+      /*exit loop when quit is entered*/
+      if(strcmp(nextWord,"quit\n")==0){
+	quit = 0;
+      }
+      /*encrypt the message by adding 8 to each individual character*/
+      int i;
+      for(i=0; i<2048; i++){
+	if(nextWord[i]=='\n'){
+	  break;
+	}
+	nextWord[i] = nextWord[i]+8;
+      }
+      /*write command to the socket to be sent to server*/
+      write(sockfd,nextWord,strlen(nextWord));
+      memset(input1,0,sizeof(input1));
+      /*read the redirected stdout and stderr from server*/
+      while(read(sockfd,buffer,1)>=0){
+	printf("%s",buffer);
+	if(strcmp(buffer,"\0")==0){
+	  break;
+	}
+      }
+      printf("\n");
     }
-    printf("\n");
   }
   return 0;
 }
--- project1/server.c
+++ project1/server.c
@@ -6,12 +6,21 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <stdbool.h>
+
+void *getProcesses(void *param);
+int PIDfd[2];
+int needPIDfd[2];
+int allPIDs[100] = {0};
 
 int main(int argc, char *argv[]){
   int listenfd, connfd;
   socklen_t addrlen;
   struct sockaddr_in myaddr;
   char buffer[2048] = {0};
+  char history[2048][10] = {"","","","","","","","","",""};
+  pthread_t PIDthread;
   /*create a new socket for the server*/
   listenfd = socket(AF_INET,SOCK_STREAM,0);
   if(listenfd<0){
@@ -32,6 +41,12 @@ int main(int argc, char *argv[]){
     perror("listening error");
     exit(1);
   }
+  /*pipe so that the parent can send the current jobs to child*/
+  pipe(PIDfd);
+  pipe(needPIDfd);
+  /*create thread that will write current processes to pipe upon a request
+    from a child process*/
+  pthread_create(&PIDthread,NULL,getProcesses,NULL);
   /*loop infinitely so that the server coninually recieves new clients*/
   for(;;){
     /*accept the next connection request*/
@@ -42,23 +57,87 @@ int main(int argc, char *argv[]){
       exit(1);
     }
     /*create new process so that there can be multiple clients*/
-    if(fork()==0){
+    int child_pid = fork();
+    if(child_pid==0){
+      close(PIDfd[1]);
       while(1){
 	/*read the user input from the socket*/
 	if(read(connfd,buffer,2048)<0){
 	  perror("read fail");
 	  exit(0);
 	}
+	/*calls on function to reap all zombie processes*/
+	write(needPIDfd[1],"zombie",sizeof("zombie"));
 	/*if user types "quit" then exit this process*/
 	if(strcmp(buffer,"quit\n")==0){
+	  write(needPIDfd[1],"quit",sizeof("quit"));
 	  close(listenfd);
 	  close(connfd);
 	  exit(0);
+	}
+	/*decrypt the message that was received by the server*/
+	int i;
+	for(i=0; i<2048; i++){
+	  if(buffer[i]=='\n'){
+	    break;
+	  }
+	  buffer[i] = buffer[i]-8;
 	}
 	/*get rid of new line from stdin so that the command is read right*/
 	if(buffer[strlen(buffer)-1]=='\n'){
 	  buffer[strlen(buffer)-1] = '\0';
 	}
+	/*translate !N command into saved command*/
+	if(buffer[0]=='!'){
+	  switch(buffer[1]){
+            case '!':
+              strcpy(buffer,history[0]);
+              break;
+            case '1':
+              if(buffer[2]=='0'){
+                strcpy(buffer,history[9]);
+              }
+              else{
+                strcpy(buffer,history[0]);
+              }
+              break;
+            case '2':
+              strcpy(buffer,history[1]);
+              break;
+            case '3':
+              strcpy(buffer,history[2]);
+              break;
+            case '4':
+              strcpy(buffer,history[3]);
+              break;
+            case '5':
+              strcpy(buffer,history[4]);
+              break;
+            case '6':
+              strcpy(buffer,history[5]);
+              break;
+            case '7':
+              strcpy(buffer,history[6]);
+              break;
+            case '8':
+              strcpy(buffer,history[7]);
+              break;
+            case '9':
+              strcpy(buffer,history[8]);
+              break;
+            default:
+              strcpy(buffer,history[0]);
+              break;
+	  }
+	}
+	/*update history by moving each input back 1 slot and adding
+	  the newst command to the front*/
+	int j;
+	for(j=8; j>=0; j--){
+	  strcpy(history[j+1],history[j]);
+	}
+	strcpy(history[0],buffer);
+	/*begin parsing command*/
 	char **allArgs = NULL;
 	char *singleArg = strtok(buffer," ");
 	int space = 0;
@@ -79,10 +158,34 @@ int main(int argc, char *argv[]){
 	  /*duplicate stdout and stderr to socket*/
 	  dup2(connfd,STDOUT_FILENO);
 	  dup2(connfd,STDERR_FILENO);
-	  /*execvp executes the command from server*/
-	  if(execvp(allArgs[0],allArgs)<0){
-	    perror("execvp failed");
-	    exit(0);
+	  /*This block first checks if the input is equal to jobs and executes
+	    jobs if it is, the if the input is history then it should execute 
+	    history, else it should execute execvp executes the command from 
+	    server*/
+	  if(strcmp(allArgs[0],"jobs")==0){
+	    int needToPrint[100] = {0};
+	    write(needPIDfd[1],"jobs",sizeof("jobs"));
+	    read(PIDfd[0],needToPrint,sizeof(needToPrint)+1);
+	    int p;
+	    for(p=0; p<100; p++){
+	      if(needToPrint[p]>0){
+		printf("%d\n",needToPrint[p]);
+	      }
+	    }
+	  }
+	  else if(strcmp(allArgs[0],"History")==0||strcmp(allArgs[0],"history")==0){
+	    int k;
+	    for(k=0; k<10; k++){
+	      if(strcmp(history[k],"")!=0){
+		printf("%d: %s\n",k+1,history[k]);
+	      }
+	    }
+	  }
+	  else{
+	    if(execvp(allArgs[0],allArgs)<0){
+	      perror("execvp failed");
+	      exit(0);
+	    }
 	  }
 	  exit(0);
 	}
@@ -94,7 +197,53 @@ int main(int argc, char *argv[]){
 	memset(buffer,0,sizeof(buffer));
       }
     }
+    else{
+      /*if this is the parent thenkeep track new child process pid for jobs*/
+      int m;
+      for(m=0; m<100; m++){
+	if(allPIDs[m]<=0){
+	  allPIDs[m]=child_pid;
+	  break;
+	}
+      }
+    }
     close(connfd);
   }
   return 0;
+}
+
+void *getProcesses(void *param){
+  /*This thread will continually check if a child process is requesting jobs 
+    and if it is then it will send back the list of pids, it will also reap 
+    reap the zombies if a child process signals it to*/
+  while(1){ 
+    char buffer[2048] = "";
+    read(needPIDfd[0],buffer,sizeof(buffer));
+    if(strcmp(buffer,"zombie")==0){
+      /*iterate through eachchild process, check if it is a zombie by accessing
+       its stat file, then waiting for it to reap the zombie*/
+      int z;
+      for(z=0; z<100; z++){
+	if(allPIDs[z]>0){
+	  bool isZombie = false;
+	  char buf[32];
+	  snprintf(buf, sizeof(buf), "/proc/%d/stat",allPIDs[z]);
+	  FILE *statFile = fopen(buf, "r");
+	  int rpid=0;
+	  char buf2[32];
+	  char rstat = 0;
+	  fscanf(statFile, "%d %30s %c", &rpid, buf2, &rstat);
+	  isZombie = (rstat=='Z');
+	  if(isZombie){
+	    waitpid(allPIDs[z],NULL,0);
+	    allPIDs[z]=0;
+	  }
+	}
+      }
+    }
+    if(strcmp(buffer,"jobs")==0){
+      write(PIDfd[1],allPIDs,sizeof(allPIDs));
+    }
+  }
+  pthread_exit(NULL);
 }
